44dd2ce410241691caff2f48d300f0e6b2c78bd5
Zeeshan <zeeshan.khan@careem.com>
2020-06-19 08:23:56 +0400

    Initial commit

---
+++ BlogDemo/AppDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.makeKeyAndVisible()
+        self.window = window
+
+        window.rootViewController = DemoListViewController()
+
+        return true
+    }
+}
+
---
+++ BlogDemo/DemoListViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class DemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
