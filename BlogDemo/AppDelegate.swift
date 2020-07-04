10c0f47d5dbe21aad1da4da35eae24e351538bba
Zeeshan <zeeshan.khan@careem.com>
2020-07-04 12:28:46 +0400

    Add support for showing toast even before app's window becomes visible

--- BlogDemo/AppDelegate.swift
+++ BlogDemo/AppDelegate.swift
@@ -6,6 +6,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        Toast.shared.show(text: "Ea voluptatibus a illo doloremque reiciendis nemo et earum. Molestiae totam voluptatibus nobis et deleniti dolores. Animi quasi ut voluptatem autem magni. Et voluptas repudiandae unde. Non sapiente maxime voluptas et facere enim.")
+
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.makeKeyAndVisible()
         self.window = window
--- BlogDemo/Toolbox/Toast.swift
+++ BlogDemo/Toolbox/Toast.swift
@@ -1,16 +1,76 @@
 import UIKit
 import Cartography
 
+private enum Constants {
+    static let animationDuration: TimeInterval = 0.3
+    static let font: UIFont = .systemFont(ofSize: 13, weight: .medium)
+    static let textSidePadding: CGFloat = 8
+    static let textTopPadding: CGFloat = 4 /// Used when status bar shows location blue var
+    static let textBottomPadding: CGFloat = 8
+    static let defaultAnchorY: CGFloat = 0.5
+    static let hiddenAnchorY: CGFloat = 1.5
+}
+
 public final class Toast {
 
-    private enum Constants {
-        static let animationDuration: TimeInterval = 0.3
-        static let font: UIFont = .systemFont(ofSize: 13, weight: .medium)
-        static let textSidePadding: CGFloat = 8
-        static let textBottomPadding: CGFloat = 8
-        static let defaultAnchorY: CGFloat = 0.5
-        static let hiddenAnchorY: CGFloat = 1.5
+    /// We need to have view controller as root of toast window as
+    /// if Toast is presented before main application window becomes active
+    private lazy var toastVC = ToastViewController()
+    private var toastWindow: ToastWindow?
+    static public let shared = Toast()
+    private init() {}
+
+    public func show(text: String, backgroundColor: UIColor = .red, textColor: UIColor = .white) {
+
+        /// Hide old window
+        toastWindow = nil
+
+        /// Setup new window
+        setupUI(text: text, backgroundColor: backgroundColor, textColor: textColor)
+
+        /// Show the toast by animating window's anchor point
+        toastWindow?.layer.anchorPoint.y = Constants.hiddenAnchorY
+        UIView.animate(withDuration: Constants.animationDuration, delay: 0, options: .curveEaseOut, animations: {
+            self.toastWindow?.layer.anchorPoint.y = Constants.defaultAnchorY
+        })
     }
+
+    public func hide() {
+        /// return if window is already nil
+        guard let window = toastWindow else { return }
+
+        /// Hide the toast by animating window's anchor point and then set nil to window to completely remove it
+        UIView.animate(withDuration: Constants.animationDuration, delay: 0, options: .curveEaseIn, animations: {
+            window.layer.anchorPoint.y = Constants.hiddenAnchorY
+        }, completion: { _ in
+            self.toastWindow = nil
+        })
+    }
+
+    private func setupUI(text: String, backgroundColor: UIColor, textColor: UIColor) {
+        toastVC.setText(text, backgroundColor: backgroundColor, textColor: textColor)
+        
+        let window = ToastWindow()
+        window.backgroundColor = .clear
+        window.isHidden = false
+        window.rootViewController = toastVC
+        toastWindow = window
+
+        /// If toast is presented before application's main window appears,
+        /// we need to set toast window level one up to make sure it overlaps the app window
+        window.windowLevel = .init(UIWindow.Level.normal.rawValue + 1)
+    }
+}
+
+private class ToastWindow: UIWindow {
+    override open func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let view = super.hitTest(point, with: event)
+        guard view != self else { return nil }
+        return view
+    }
+}
+
+private class ToastViewController: UIViewController {
 
     private lazy var toastLabel: UILabel = {
         let label = UILabel()
@@ -37,57 +97,30 @@ public final class Toast {
         return view
     }()
 
-    private var toastWindow: ToastWindow?
-    static public let shared = Toast()
-    private init() {}
-
-    public func show(text: String, backgroundColor: UIColor = .red, textColor: UIColor = .white) {
-
-        /// Hide old window
-        toastWindow = nil
-
-        /// Setup new window
-        setupUI(text: text, backgroundColor: backgroundColor, textColor: textColor)
-
-        /// Show the toast by animating view's anchor point
-        toastView.layer.anchorPoint.y = Constants.hiddenAnchorY
-        UIView.animate(withDuration: Constants.animationDuration, delay: 0, options: .curveEaseOut, animations: {
-            self.toastView.layer.anchorPoint.y = Constants.defaultAnchorY
-        })
+    open override func loadView() {
+        view = OverlayView()
     }
 
-    public func hide() {
-        /// return if window is already nil
-        guard toastWindow != nil else { return }
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-        /// Hide the toast by animating view's anchor point and then set nil to window to completely remove it
-        UIView.animate(withDuration: Constants.animationDuration, delay: 0, options: .curveEaseIn, animations: {
-            self.toastView.layer.anchorPoint.y = Constants.hiddenAnchorY
-        }, completion: { _ in
-            self.toastWindow = nil
-        })
+        view.addSubview(toastView)
+        constrain(view, toastView) { container, toast in
+            toast.top == container.top
+            toast.leading == container.leading
+            toast.trailing == container.trailing
+        }
     }
 
-    private func setupUI(text: String, backgroundColor: UIColor, textColor: UIColor) {
+    fileprivate func setText(_ text: String, backgroundColor: UIColor, textColor: UIColor) {
         toastLabel.text = text
         toastLabel.textColor = textColor
         toastLabel.backgroundColor = backgroundColor
         toastView.backgroundColor = backgroundColor
-
-        let window = ToastWindow()
-        window.backgroundColor = .clear
-        window.isHidden = false
-        window.addSubview(toastView)
-        constrain(window, toastView) { window, view in
-            view.top == window.top
-            view.leading == window.leading
-            view.trailing == window.trailing
-        }
-        toastWindow = window
     }
 }
 
-private class ToastWindow: UIWindow {
+private class OverlayView: UIView {
     override open func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         let view = super.hitTest(point, with: event)
         guard view != self else { return nil }
